b64442c1e0187524e9879d7cbaad8cf96b400c4e
alaafawzy <alaaf9443@gmail.com>
2025-09-05 10:12:01 +0300

    improve constant class

--- app/src/main/java/com/example/calculator/Constant.kt
+++ app/src/main/java/com/example/calculator/Constant.kt
@@ -2,9 +2,9 @@ package com.example.calculator
 
 object Constant {
     val NUMBER="0".."9"
-    val DOT="DOT"
-    val Clear ="CLEAR"
-    val BACKSPACE="BACKSPACE"
-    val EQUAL="EQUAL"
-    val NEG="NEGATE"
+    const val DOT="DOT"
+    const val Clear ="CLEAR"
+    const val BACKSPACE="BACKSPACE"
+    const val EQUAL="EQUAL"
+    const val NEG="NEGATE"
 }
