25b27408ad0ddedecddeff8ba46d0e161c8f2e19
alaafawzy <alaaf9443@gmail.com>
2025-09-05 10:07:51 +0300

    add constant class

---
+++ app/src/main/java/com/example/calculator/Constant.kt
@@ -0,0 +1,10 @@
+package com.example.calculator
+
+object Constant {
+    val NUMBER="0".."9"
+    val DOT="DOT"
+    val Clear ="CLEAR"
+    val BACKSPACE="BACKSPACE"
+    val EQUAL="EQUAL"
+    val NEG="NEGATE"
+}
